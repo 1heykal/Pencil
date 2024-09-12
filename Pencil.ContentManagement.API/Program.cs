584a2fe2d23bd27a89652a63dc3eadbd2bba53d4
Osama Heykal <oneheykal@gmail.com>
2024-09-12 19:47:27 +0300

    feat(config): Added Program.cs config (Swagger, Auth, etc).

--- Pencil.ContentManagement.API/Program.cs
+++ Pencil.ContentManagement.API/Program.cs
@@ -1,6 +1,87 @@
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Pencil.ContentManagement.API;
+using Pencil.ContentManagement.API.Filters;
+using Pencil.ContentManagement.API.Middlewares;
+using Pencil.ContentManagement.Application;
+using Pencil.ContentManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    setupAction.AddSecurityDefinition("Pencil.ContentManagement.API.BearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Input a valid token to access this API"
+    });
+
+    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Pencil.ContentManagement.API.BearerAuth",
+                }
+            },
+            new List<string>()
+        }
+    });
+    
+});
+
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("../swagger/v1/swagger.json", "Pencil.ContentManagement.API V1");
+    s.RoutePrefix = string.Empty;
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.ConfigureStaticFiles();
+
+app.MapGet("/helloworld", () => "Hello World!");
 
 app.Run();
