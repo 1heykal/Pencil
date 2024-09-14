0722b2eb6c1c44aff7736bbcd823903cd02837b8
Osama Heykal <oneheykal@gmail.com>
2024-09-14 20:01:09 +0300

    refactor(Filters:GetUserId): Added logic to keep user Id.

--- Pencil.ContentManagement.API/Filters/GetUserId.cs
+++ Pencil.ContentManagement.API/Filters/GetUserId.cs
@@ -1,24 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Pencil.ContentManagement.Application.Responses;
 
 namespace Pencil.ContentManagement.API.Filters;
 
-public class CheckUserIdAttribute : ActionFilterAttribute
-{
+public class GetUserIdAttribute : ActionFilterAttribute
+{    
     public override void OnActionExecuting(ActionExecutingContext context)
     {
         var success = Guid.TryParse(context.HttpContext.User?.FindFirst("UserId")?.Value, out var result);
 
         if (!success)
-        {
-            context.Result = new UnauthorizedResult();
-        }
+            context.Result = new UnauthorizedResponse<string>();
+        
+        context.HttpContext.Items["UserId"] = result;
     }
-
-    public void OnActionExecuted(ActionExecutedContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-   
 }
